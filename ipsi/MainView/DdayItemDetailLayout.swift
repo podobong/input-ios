04716b25c0efb09822b491952b5a916b99982d13
podobong <kim11560@snu.ac.kr>
2020-07-30 11:57:03 +0900

    Edited DdayLayout

--- ipsi/MainView/DdayItemDetailLayout.swift
+++ ipsi/MainView/DdayItemDetailLayout.swift
@@ -8,7 +8,7 @@ struct DdayItemDetailLayout: View {
     var body: some View {
         let schedules: [Schedule] = self.selected.old[self.index].schedules
         let formatter: DateFormatter = DateFormatter()
-        formatter.dateFormat = "M월 d일"
+        formatter.dateFormat = "YYYY년 M월 d일"
         
         return VStack(spacing: 0) {
             ForEach(0..<schedules.count, id: \.self) { i in
@@ -20,23 +20,27 @@ struct DdayItemDetailLayout: View {
                         Spacer()
                         VStack(spacing: 0) {
                             HStack(spacing: 0) {
-                                Text("시작일: ")
-                                    .font(.system(size: 14))
-                                    .foregroundColor(Color("DarkGray"))
                                 Text(formatter.string(from: schedules[i].startDate))
+                                    .frame(minWidth: 0, maxWidth: .infinity, alignment: .trailing)
+                                Spacer().frame(width: 2)
+                                Text("부터")
+                                    .font(.system(size: 15))
+                                    .foregroundColor(Color("DarkDarkGray"))
                             }
-                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .trailing)
+                            Spacer().frame(height: 5)
                             HStack(spacing: 0) {
-                                Text("마감일: ")
-                                    .font(.system(size: 14))
-                                    .foregroundColor(Color("DarkGray"))
-                                Text(formatter.string(from: schedules[i].endDate))
+                                Text("~ \(formatter.string(from: schedules[i].endDate))")
+                                    .frame(minWidth: 0, maxWidth: .infinity, alignment: .trailing)
+                                Spacer().frame(width: 2)
+                                Text("까지")
+                                    .font(.system(size: 15))
+                                    .foregroundColor(Color("DarkDarkGray"))
                             }
-                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .trailing)
+                            .frame(alignment: .bottom)
                         }
                     }
-                        .padding(.horizontal, 20)
-                        .padding(.vertical, 16)
+                    .padding(.vertical, 16)
+                    .padding(.horizontal, 20)
                     Rectangle()
                         .fill(Color("MiddleGray"))
                         .frame(height: 0.5)
